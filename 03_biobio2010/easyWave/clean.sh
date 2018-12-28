6481b57c412326985ef8e31fead31068e5405686
José  Galaz <jdgalazm@gmail.com>
2018-12-28 14:51:25 -0300

    files for running easywave

---
+++ 03_biobio2010/easyWave/clean.sh
@@ -0,0 +1,5 @@
+rm -rf eWave*
+rm -rf easywave.log
+rm -rf error.msg
+rm -rf *.png
+rm -rf *.lst
---
+++ 03_biobio2010/easyWave/run.sh
@@ -0,0 +1,1 @@
+../../easyWave/code/src/easywave -grid ../data/bathymetryEasywave.grd -source ../data/fault.flt -time 1500 -poi pois -poi_report 
