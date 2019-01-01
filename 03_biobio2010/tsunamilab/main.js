2d4d7693aeab476334b6825f4cbc5490cbb9972b
José  Galaz <jdgalazm@gmail.com>
2019-01-01 18:05:31 -0300

    good results with coarse bathymetry without antarctic full domain

--- 03_biobio2010/tsunamilab/main.js
+++ 03_biobio2010/tsunamilab/main.js
@@ -49,53 +49,57 @@ let colormap = {
 // let w = 1334;
 // let h = 934;
 
+// 10 min
+let w = 1415;
+let h = 780;
+
 // small 5min
-let w = 1050;
-let h = 850;
+// let w = 1050;
+// let h = 850;
 
 
 let data = {
-    // xmin : 90, // full domain
-    // xmax :  290,
-    // ymin :  -70,
-    // ymax : 70,
-    xmin: -121.67+360,
-    xmax: -34.17+360,
-    ymin: -77.5,
-    ymax: -6.67,
+    xmin : 90, // full domain
+    xmax :  325.83,
+    ymin :  -60,
+    ymax : 70,
+    // xmin: -121.67+360,
+    // xmax: -34.17+360,
+    // ymin: -77.5,
+    // ymax: -6.67,
     waveWidth: w,
     waveHeight: h,
     coordinates: 'spherical',
-    // bathymetry: '../data/bathymetry12min',
-    bathymetry: '../data/bathymetry_small5min',
-    binaryBathymetry: false,
+    bathymetry: '../data/bathymetry10min',
+    // bathymetry: '../data/bathymetry_small5min',
+    binaryBathymetry: true,
     earthquake: '../data/earthquake.csv',
 }
 
 let output = {
     colormap: colormap,
-    displayWidth:  w,
-    displayHeight: h,
+    displayWidth:  w/2,
+    displayHeight: h/2,
     stopTime: 60*60*25,
     displayOption: 'heights',
     pois:{
-        // '21414': {location:[178.219,48.968]},
-        // '21419': {location:[155.717,44.435]},
-        // '21401': {location:[152.583,42.617]},
-        // '21418': {location:[148.645,38.727]},
-        // '21413': {location:[152.132,30.533]},
-        // '52403': {location:[145.52,4.02]},
-        // '52406': {location:[164.977,-5.307]},
-        // '55012': {location:[158.453,-15.664]},
-        // '51425': {location:[183.68,-9.517]},
-        // '51407': {location:[203.455,19.57]},
-        // '46404': {location:[231.267,45.853]},
-        // '46407': {location:[231.168,42.682]},
-        // '46411': {location:[232.933,39.333]},
-        // '46412': {location:[239.437,32.492]},
-        // '43412': {location:[253.03300000000002,16.045]},
-        // '43413': {location:[259.91700000000003,11.012]},
-        // '32411': {location:[269.12-360,4.953]},
+        '21414': {location:[178.219,48.968]},
+        '21419': {location:[155.717,44.435]},
+        '21401': {location:[152.583,42.617]},
+        '21418': {location:[148.645,38.727]},
+        '21413': {location:[152.132,30.533]},
+        '52403': {location:[145.52,4.02]},
+        '52406': {location:[164.977,-5.307]},
+        '55012': {location:[158.453,-15.664]},
+        '51425': {location:[183.68,-9.517]},
+        '51407': {location:[203.455,19.57]},
+        '46404': {location:[231.267,45.853]},
+        '46407': {location:[231.168,42.682]},
+        '46411': {location:[232.933,39.333]},
+        '46412': {location:[239.437,32.492]},
+        '43412': {location:[253.03300000000002,16.045]},
+        '43413': {location:[259.91700000000003,11.012]},
+        '32411': {location:[269.12-360,4.953]},
         '32412': {location:[273.626,-17.984]},
         '32413': {location:[266.483,-7.406]},
         '32401': {location:[286.579,-20.474]},
