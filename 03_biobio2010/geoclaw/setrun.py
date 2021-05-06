9edb0ca43030b9b3cac594ee04733d28620620bb
Jose Galaz <jdgalaz@uc.cl>
2021-05-06 11:37:24 +0200

    add gauges & reduce resolution

--- 03_biobio2010/geoclaw/setrun.py
+++ 03_biobio2010/geoclaw/setrun.py
@@ -288,9 +288,9 @@ def setrun(claw_pkg='geoclaw'):
     amrdata.amr_levels_max = 2
 
     # List of refinement ratios at each level (length at least mxnest-1)
-    amrdata.refinement_ratios_x = [20,6]
-    amrdata.refinement_ratios_y = [20,6]
-    amrdata.refinement_ratios_t = [20,6]
+    amrdata.refinement_ratios_x = [2,6]
+    amrdata.refinement_ratios_y = [2,6]
+    amrdata.refinement_ratios_t = [2,6]
 
 
     # Specify type of each aux variable in amrdata.auxtype.
@@ -348,9 +348,42 @@ def setrun(claw_pkg='geoclaw'):
     # ---------------
     # Gauges:
     # ---------------
-    rundata.gaugedata.gauges = []
+
+    gauges = {
+        "21414": {"location": [178.219, 48.968]},
+        "21419": {"location": [155.717, 44.435]},
+        "21401": {"location": [152.583, 42.617]},
+        "21418": {"location": [148.645, 38.727]},
+        "21413": {"location": [152.132, 30.533]},
+        "52403": {"location": [145.52, 4.02]},
+        "52406": {"location": [164.977, -5.307]},
+        "55012": {"location": [158.453, -15.664]},
+        "51425": {"location": [183.68, -9.517]},
+        "51407": {"location": [203.455, 19.57]},
+        "46404": {"location": [231.267, 45.853]},
+        "46407": {"location": [231.168, 42.682]},
+        "46411": {"location": [232.933, 39.333]},
+        "46412": {"location": [239.437, 32.492]},
+        "43412": {"location": [253.03300000000002, 16.045]},
+        "43413": {"location": [259.91700000000003, 11.012]},
+        "32411": {"location": [269.12 - 360, 4.953]},
+        "32412": {"location": [273.626, -17.984]},
+        "32413": {"location": [266.483, -7.406]},
+        "32401": {"location": [286.579, -20.474]},
+    }
+
+
     # for gauges append lines of the form  [gaugeno, x, y, t1, t2]
-    rundata.gaugedata.gauges.append([32412, -86.392 + 360, -17.975, 0., 1.e10])
+    rundata.gaugedata.gauges = [
+        [
+            int(gauge_number),
+            gauges[gauge_number]["location"][0],
+            gauges[gauge_number]["location"][1],
+            0.0,
+            1e10,
+        ]
+        for gauge_number in gauges
+    ]
     
 
     return rundata
@@ -390,7 +423,7 @@ def setgeo(rundata):
     # Refinement settings
     refinement_data = rundata.refinement_data
     refinement_data.variable_dt_refinement_ratios = True
-    refinement_data.wave_tolerance = 5.e-2
+    refinement_data.wave_tolerance = 1.e-2
     refinement_data.deep_depth = 1e2
     refinement_data.max_level_deep = 3
 
