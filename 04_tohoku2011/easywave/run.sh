a7700402469bbf0aedc4568a969907ab8a97d64c
Jose Galaz <jdgalaz@uc.cl>
2021-05-13 12:32:56 +0200

    run easywave at full resolution

--- 04_tohoku2011/easywave/run.sh
+++ 04_tohoku2011/easywave/run.sh
@@ -3,4 +3,4 @@ rm -rf *.log
 rm -rf eWave*
 rm -rf error.msg
 # easywave -grid ../../03_biobio2010/data/bathymetryEasywave_3min.grd -source ../data/fault.flt  -time 1500 -poi ../data/pois.txt -poi_report
-easywave -grid ../../03_biobio2010/data/bathymetryEasywave_10min.grd -source ../data/fault.flt  -time 1500 -poi ../data/pois.txt -poi_report
+easywave -grid ../../03_biobio2010/data/bathymetryEasywave_3min.grd -source ../data/fault.flt  -time 1500 -poi ../data/pois.txt -poi_report
