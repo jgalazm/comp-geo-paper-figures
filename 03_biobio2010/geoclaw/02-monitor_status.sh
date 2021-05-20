fe368e33d4b00d586ed6aadc3005e21726b2c274
Jose Galaz <jdgalaz@uc.cl>
2021-05-20 10:42:28 +0200

    dont use tail to monitor progress

--- 03_biobio2010/geoclaw/02-monitor_status.sh
+++ 03_biobio2010/geoclaw/02-monitor_status.sh
@@ -3,5 +3,3 @@ ssh jdgalaz@mazinger.ing.puc.cl 'squeue -o "%.10u %.10i %.10P %.20j %.11M %.10T 
 read -p "Press enter to print logs"
 job_number=`cat job_number.log | grep -o "[0-9.]\+"`
 ssh jdgalaz@mazinger.ing.puc.cl "cat work/03_biobio2010/geoclaw/slurm-"$job_number".out"
-echo "tail..."
-ssh jdgalaz@mazinger.ing.puc.cl tail -f work/03_biobio2010/geoclaw/slurm-$job_number.out
